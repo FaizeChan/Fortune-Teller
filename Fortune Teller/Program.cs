3d1caf7919d95786d90ba8fa6957e793ad5c1fef
FaizeChan <obey.faize@gmail.com>
2017-09-30 18:02:14 -0400

    I'm almost done!

--- Fortune Teller/Program.cs
+++ Fortune Teller/Program.cs
@@ -23,12 +23,12 @@ namespace Fortune_Teller
             int userAge = int.Parse(Console.ReadLine());
             Console.WriteLine("\n");
 
-            Console.WriteLine("Simply Amazing, but I'm a little curious... \nIn number form, what's your birthday month?");
+            Console.WriteLine("Oh you're" + " " + userAge + " " + "That's wassup, but I'm a little curious... \nIn number form, what's your birthday month?");
             int birthMonth = int.Parse(Console.ReadLine());
             Console.WriteLine("\n");
 
             Console.WriteLine("Aye Turn Up! What's your favorite ROYGBIV color? \nIf you don't know the ROYGBIV colors just enter (help)");
-            string favColor = Console.ReadLine().ToLower();
+            string favColor = Console.ReadLine().ToLower().Trim();
             string userHelp = "help";
             Console.WriteLine("\n");
             if (favColor == userHelp)
@@ -41,22 +41,35 @@ namespace Fortune_Teller
             Console.WriteLine("Interesting indeed! How many siblings do you have?");
             int siblingNum = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("\nPerfect, I believe I have enough information to declare your fortune!\nAre you ready? Yes, or No");
-            string readyUp = Console.ReadLine().ToLower();
+            Console.WriteLine("\nPerfect, I believe I have enough information to declare your fortune!\nAre you ready? Not like you have a choice in the matter Mwahaha \nType in either(Yes, or No)");
+            string readyUp = Console.ReadLine().ToLower().Trim();
             string agree = "yes";
+            string disagree = "no";
             Console.WriteLine();
             if (readyUp == agree)
             {
                 Console.WriteLine("\nExpectooo Patronum!!");
             }
+            else if (readyUp == disagree)
+            {
+                Console.WriteLine("\nI said you don't have a choice! You're getting your fortune otherwise \nI don't get paid!");
+            }
+            else 
+            {
+                Console.WriteLine("That's not one of the flipping options. Don't be scared of your fortune \nWell since you didnt say no here's your fortune anyways hahaha!");
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Your name is" + " " + firstName + " " + lastName + ".");
+            if(userAge%2==0)
+            {
+                Console.WriteLine("You will retire in 20 years pretty decent.");
+            }
             else
             {
-                Console.WriteLine("\nNo fortune? Urghh! I don't get paid enough for this");
+                Console.WriteLine("You will retire in 15 years Nice!");
             }
-
-
-
-
+                    
         }
     }
 }
