5e80106cd3799446492b529261d5d3bb21a2e674
FaizeChan <obey.faize@gmail.com>
2017-09-30 21:06:36 -0400

    slow and steady

--- Fortune Teller/Program.cs
+++ Fortune Teller/Program.cs
@@ -10,12 +10,15 @@ namespace Fortune_Teller
     {
         static void Main(string[] args)
         {
+
+            //Warning it may not be the prettiest or cleanist code but forgive my newb-ness i'm still learning'
+
             Console.WriteLine("\n");
             Console.WriteLine("Let's get this party started! I am the Great Fortune Teller FaizeChan! \nWhat's your first name?");
             string firstName = Console.ReadLine();
             Console.WriteLine("\n");
 
-            Console.WriteLine("Wow, nice name! You're doing Great! What's your last name?");
+            Console.WriteLine("Well Hello" + " " + firstName + "! What's your last name?");
             string lastName = Console.ReadLine();
             Console.WriteLine("\n");
 
@@ -25,15 +28,29 @@ namespace Fortune_Teller
 
             Console.WriteLine("Oh you're" + " " + userAge + " " + "That's wassup, but I'm a little curious... \nIn number form, what's your birthday month?");
             int birthMonth = int.Parse(Console.ReadLine());
+            int one = 1;
+            int two = 2;
+            int three = 3;
+            int four = 4;
+            int five = 5;
+            int six = 6;
+            int seven = 7;
+            int eight = 8;
+            int nine = 9;
+            int ten = 10;
+            int eleven = 11;
+            int twelve = 12;            
+
             Console.WriteLine("\n");
 
             Console.WriteLine("Aye Turn Up! What's your favorite ROYGBIV color? \nIf you don't know the ROYGBIV colors just enter (help)");
             string favColor = Console.ReadLine().ToLower().Trim();
             string userHelp = "help";
             Console.WriteLine("\n");
+
             if (favColor == userHelp)
             {
-                Console.WriteLine("Aye that's fine. ROYGBIV is the following colors. \nRed \nOrange \nYellow \nGreen \nBlue \nIndigo \nViolit \nAlright so now which one of these colors is your favorite one?");
+                Console.WriteLine("Aye that's fine. ROYGBIV is the following colors. \nRed \nOrange \nYellow \nGreen \nBlue \nIndigo \nViolit \n \nAlright so now which one of these colors is your favorite one?");
                 favColor = Console.ReadLine();
                 Console.WriteLine("\n");
             }
@@ -50,6 +67,8 @@ namespace Fortune_Teller
             {
                 Console.WriteLine("\nExpectooo Patronum!!");
             }
+            //When did the Fortune Teller FaizeChan turn into a wizard from Hogwarts???
+
             else if (readyUp == disagree)
             {
                 Console.WriteLine("\nI said you don't have a choice! You're getting your fortune otherwise \nI don't get paid!");
@@ -59,17 +78,71 @@ namespace Fortune_Teller
                 Console.WriteLine("That's not one of the flipping options. Don't be scared of your fortune \nWell since you didnt say no here's your fortune anyways hahaha!");
             }
             Console.WriteLine("\n");
-
-            Console.WriteLine("Your name is" + " " + firstName + " " + lastName + ".");
             if(userAge%2==0)
             {
-                Console.WriteLine("You will retire in 20 years pretty decent.");
+                Console.WriteLine("Your name is" + " " + firstName + " " + lastName + " " + "and you will retire in 20 years pretty decent.");
             }
             else
             {
-                Console.WriteLine("You will retire in 15 years Nice!");
+                Console.WriteLine("Your name is" + " " + firstName + " " + lastName + " " + "and you will retire in 15 years Nice!");
             }
-                    
+             //I know there is an easier way to do all this but I don't know it so for now I'm going to stick with the hard way because it's working     
+             
+            if (birthMonth == one)
+            {
+                Console.WriteLine("Ooooo, you will also have a staggering amount of 2 Million dollars in the bank");
+            }
+            else if (birthMonth == two)
+            {
+                Console.WriteLine("Ooooo, you will also have a staggering amount of 2 Million dollars in the bank");
+            }
+            else if (birthMonth == three)
+            {
+                Console.WriteLine("Ooooo, you will also have a staggering amount of 2 Million dollars in the bank");
+            }
+            else if (birthMonth == four)
+            {
+                Console.WriteLine("Ooooo, you will also have a staggering amount of 2 Million dollars in the bank");
+            }
+            else if (birthMonth == five)
+            {
+                Console.WriteLine("Pretty nice, you will also have a wopping amount of $500,000.00 in the bank");
+            }
+            else if (birthMonth == six)
+            {
+                Console.WriteLine("Pretty nice, you will also have a wopping amount of $500,000.00 in the bank");
+            }
+            else if (birthMonth == seven)
+            {
+                Console.WriteLine("Pretty nice, you will also have a wopping amount of $500,000.00 in the bank");
+            }
+            else if (birthMonth == eight)
+            {
+                Console.WriteLine("Pretty nice, you will also have a wopping amount of $500,000.00 in the bank");
+            }
+            else if (birthMonth == nine)
+            {
+                Console.WriteLine("You will also have a deceant amount of $20,000.00 in the bank");
+            }
+            else if (birthMonth == ten)
+            {
+                Console.WriteLine("You will also have a deceant amount of $20,000.00 in the bank");
+            }
+            else if (birthMonth == eleven)
+            {
+                Console.WriteLine("You will also have a deceant amount of $20,000.00 in the bank");
+            }
+            else if (birthMonth == twelve)
+            {
+                Console.WriteLine("You will also have a deceant amount of $20,000.00 in the bank");
+            }
+            else
+            {
+                Console.WriteLine("tsk tsk... but sadly you will have $0.00 in the bank");
+            }
+
+
+
         }
     }
 }
